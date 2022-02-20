3104eff6fd9d4743976eb93a482474eff1251f9d
sosoariar <sosoariar@163.com>
2022-02-20 17:59:12 +0800

    函数式编程

--- java8-case/src/main/java/com/soso/MinDemo.java
+++ java8-case/src/main/java/com/soso/MinDemo.java
@@ -1,11 +1,14 @@
 package com.soso;
 
+import java.util.stream.IntStream;
+
 public class MinDemo {
 
     public static void main(String[] args) {
 
         int[] nums = {5,4,3,2,1};
 
+        // 命令式
         int min = Integer.MAX_VALUE;
 
         for(int i:nums){
@@ -15,5 +18,8 @@ public class MinDemo {
         }
         System.out.println(min);
 
+        // 函数式
+        int min2 = IntStream.of(nums).min().getAsInt();
+        System.out.println(min2);
     }
 }
