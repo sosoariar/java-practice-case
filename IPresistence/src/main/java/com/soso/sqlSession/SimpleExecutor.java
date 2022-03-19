404ca36fb00e07f22228f3754a9f77e21a2e4f87
sosoariar <sosoariar@163.com>
2022-03-19 22:35:31 +0800

    如何获取数据库链接

--- IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
+++ IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
@@ -3,12 +3,17 @@ package com.soso.sqlSession;
 import com.soso.pojo.Configuration;
 import com.soso.pojo.MappedStatement;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SimpleExecutor implements Executor{
 
     @Override
-    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object... params) {
+    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object... params) throws SQLException {
+
+        Connection connection = configuration.getDataSource().getConnection();
+
         return null;
     }
 
