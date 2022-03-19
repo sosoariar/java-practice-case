bae83903e3e38de3cf25450455c9c88dfcc1df79
sosoariar <sosoariar@163.com>
2022-03-19 21:54:01 +0800

    客户端测试代码更新

--- IPresistece_test/src/test/java/com/soso/io/TestPresistence.java
+++ IPresistece_test/src/test/java/com/soso/io/TestPresistence.java
@@ -1,13 +1,27 @@
 package com.soso.io;
 
+import com.soso.bean.User;
+import com.soso.sqlSession.SqlSession;
+import com.soso.sqlSession.SqlSessionFactory;
+import com.soso.sqlSession.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import java.io.InputStream;
 
 public class TestPresistence {
 
     @Test
-    public void test(){
+    public void test() throws Exception {
+
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user01 = new User();
+        user01.setId(1);
+        user01.setUsername("zhangsan");
+
+        User user02 = sqlSession.selectOne("user.selectOne",user01);
+
     }
 
 }
