356bd954e33941c3981c5bc4fd7d13e943e07e05
sosoariar <sosoariar@163.com>
2022-03-19 19:37:17 +0800

    Configuration 解析

---
+++ IPresistence/src/main/java/com/soso/config/XMLConfigBuilder.java
@@ -0,0 +1,50 @@
+package com.soso.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.soso.pojo.Configuration;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+public class XMLConfigBuilder {
+
+    private Configuration configuration;
+
+    public XMLConfigBuilder() {
+        configuration = new Configuration();
+    }
+
+
+    public Configuration parseConfig(InputStream inputStream) throws Exception {
+
+        Document document = new SAXReader().read(inputStream);
+        Element rootElement = document.getRootElement();
+        List<Element> list = rootElement.selectNodes("//property");
+        Properties properties = new Properties();
+
+        for(Element element:list){
+            String name = element.attributeValue("name");
+            String value = element.attributeValue("value");
+            properties.setProperty(name,value);
+        }
+
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setDriverClass(properties.getProperty("driverClass"));
+        comboPooledDataSource.setJdbcUrl(properties.getProperty("jdbcUrl"));
+        comboPooledDataSource.setUser(properties.getProperty("username"));
+        comboPooledDataSource.setPassword(properties.getProperty("password"));
+
+        configuration.setDataSource(comboPooledDataSource);
+
+        // mapper.xml 解析
+
+
+        return null;
+    }
+
+}
