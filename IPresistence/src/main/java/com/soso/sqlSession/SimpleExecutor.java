5cc7331e9db896db96c5aeddf38d5e745ea7d615
sosoariar <sosoariar@163.com>
2022-03-19 23:28:06 +0800

    SQL 占位符解析过程

--- IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
+++ IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
@@ -1,7 +1,12 @@
 package com.soso.sqlSession;
 
+import com.soso.config.BoundSql;
 import com.soso.pojo.Configuration;
 import com.soso.pojo.MappedStatement;
+import com.soso.utils.GenericTokenParser;
+import com.soso.utils.ParameterMapping;
+import com.soso.utils.ParameterMappingTokenHandler;
+import com.soso.utils.TokenHandler;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,8 +18,26 @@ public class SimpleExecutor implements Executor{
     public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object... params) throws SQLException {
 
         Connection connection = configuration.getDataSource().getConnection();
+        String sql = mappedStatement.getSql();
+
+        BoundSql boundSql = getBoundSql(sql);
 
         return null;
     }
+
+    private BoundSql getBoundSql(String sql) {
+
+        // 占位符处理
+        ParameterMappingTokenHandler parameterMappingTokenHandler = new ParameterMappingTokenHandler();
+        GenericTokenParser genericTokenParser = new GenericTokenParser("#{","}",parameterMappingTokenHandler);
+
+        String parse = genericTokenParser.parse(sql);
+        List<ParameterMapping> parameterMappings = parameterMappingTokenHandler.getParameterMappings();
+
+        BoundSql boundSql = new BoundSql(parse,parameterMappings);
+
+        return boundSql;
+    }
+
 
 }
