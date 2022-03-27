613f66d9227667611cc54961ddb2f088d3d23d6b
sosoariar <sosoariar@163.com>
2022-03-27 11:02:34 +0800

    statement 将所有的应用逻辑串联起来

--- clean_code/src/main/java/com/soso/Movie.java
+++ clean_code/src/main/java/com/soso/Movie.java
@@ -14,21 +14,22 @@ public class Movie {
         this._priceCode = _priceCode;
     }
 
-    public String get_title() {
+    public String getTitle() {
         return _title;
     }
 
-    public void set_title(String _title) {
+    public void setTitle(String _title) {
         this._title = _title;
     }
 
-    public int get_priceCode() {
+    public int getPriceCode() {
         return _priceCode;
     }
 
-    public void set_priceCode(int _priceCode) {
+    public void setPriceCode(int _priceCode) {
         this._priceCode = _priceCode;
     }
+
 }
 
 
