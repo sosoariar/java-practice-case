81b1b569ac714a36c603d955b79eae494e9a81e1
sosoariar <sosoariar@163.com>
2022-03-27 13:16:57 +0800

    拆分statement 重构后函数的局部变量可以修改

--- clean_code/src/main/java/com/soso/Customer.java
+++ clean_code/src/main/java/com/soso/Customer.java
@@ -56,29 +56,36 @@ public class Customer {
         return result;
     }
 
-    private int amountFor(Rental each){
+    /**
+     * 重构过程中,返回类型发生了错误,但是因为double的结果自动转化为int而没有发现
+     * 重构后函数的局部变量可以修改
+     * @param aRental
+     * @return
+     */
+    private double amountFor(Rental aRental){
 
-        int thisAmount = 0;
+        double result = 0;
 
-        switch(each.getMovie().getPriceCode()){
+        switch(aRental.getMovie().getPriceCode()){
             case Movie.REGULAR:
-                thisAmount += 2;
-                if(each.getDaysRented()>2){
-                    thisAmount += (each.getDaysRented()-2)*1.5;
+                result += 2;
+                if(aRental.getDaysRented()>2){
+                    result += (aRental.getDaysRented()-2)*1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented()*3;
+                result += aRental.getDaysRented()*3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if(each.getDaysRented()>3){
-                    thisAmount += (each.getDaysRented()-3)*1.5;
+                result += 1.5;
+                if(aRental.getDaysRented()>3){
+                    result += (aRental.getDaysRented()-3)*1.5;
                 }
                 break;
         }
 
-        return thisAmount;
+        return result;
 
     }
+
 }
