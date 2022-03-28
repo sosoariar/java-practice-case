a4df794130efee75c0f6edd4ea4d5ed0ff01677f
sosoariar <sosoariar@163.com>
2022-03-28 13:16:48 +0800

    拆分statement 重构, 简化函数的内部的临时变量

--- clean_code/src/main/java/com/soso/Customer.java
+++ clean_code/src/main/java/com/soso/Customer.java
@@ -32,22 +32,30 @@ public class Customer {
      */
     public String statement(){
 
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while(rentals.hasMoreElements()){
 
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
             result += "\t"+each.getMovie().getTitle()+"\t"+String.valueOf(each.getCharge())+"\n";
 
         }
 
         result += "Amount owed is " + String.valueOf(getTotalCharge())+"\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
 
+    }
+
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
     }
 
     private double getTotalCharge(){
