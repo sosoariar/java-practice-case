e8f9f448cb9c768aadd849b06c8058bfa284fad8
sosoariar <sosoariar@163.com>
2022-03-28 17:46:28 +0800

    引入继承类型 getFrequentRenterPoints 拆分

--- clean_code/src/main/java/com/soso/ChildrensPrice.java
+++ clean_code/src/main/java/com/soso/ChildrensPrice.java
@@ -1,7 +1,17 @@
 package com.soso;
 
 public class ChildrensPrice extends Price{
+
     int getPriceCode(){
         return Movie.CHILDRENS;
     }
+
+    public double getCharge(int daysRented){
+        double result = 1.5;
+        if(daysRented>3){
+            result += (daysRented-3)*1.5;
+        }
+        return result;
+    }
+
 }
--- clean_code/src/main/java/com/soso/Price.java
+++ clean_code/src/main/java/com/soso/Price.java
@@ -1,5 +1,16 @@
 package com.soso;
 
 public abstract class Price {
+
     abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented){
+        if((getPriceCode() == Movie.NEW_RELEASE) && daysRented>1){
+            return 2;
+        }else{
+            return 1;
+        }
+    }
 }
