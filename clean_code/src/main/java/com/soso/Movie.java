b102674e92939cbfb8ab245c45d96e65252e3776
sosoariar <sosoariar@163.com>
2022-03-28 15:59:47 +0800

    运用多态取代与价格相关的条件逻辑

--- clean_code/src/main/java/com/soso/Movie.java
+++ clean_code/src/main/java/com/soso/Movie.java
@@ -30,6 +30,37 @@ public class Movie {
         this._priceCode = _priceCode;
     }
 
+    /**
+     * 租期长度作为参数传递进去,租期长度来自 Rental 对象
+     * 计算费用时需要两项数据: 租期长度和影片类型
+     * 为什么选择将租期长度传给 Movie 对象实例
+     * 因为系统中可能发生的变化是加入新影片类型
+     *
+     * @param daysRented
+     * @return
+     */
+    double getCharge(int daysRented){
+        double result = 0;
+        switch(getPriceCode()){
+            case Movie.REGULAR:
+                result += 2;
+                if(daysRented > 2){
+                    result += (daysRented - 2)*1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented*3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(daysRented>3){
+                    result += (daysRented-3)*1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
 }
 
 
