8001ec8b6e136a5b75f2e8b3cd90d385d32f859d
sosoariar <sosoariar@163.com>
2022-03-28 17:47:12 +0800

    抽象类执行默认行为

--- clean_code/src/main/java/com/soso/Price.java
+++ clean_code/src/main/java/com/soso/Price.java
@@ -7,10 +7,6 @@ public abstract class Price {
     abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented){
-        if((getPriceCode() == Movie.NEW_RELEASE) && daysRented>1){
-            return 2;
-        }else{
-            return 1;
-        }
+        return 1;
     }
 }
