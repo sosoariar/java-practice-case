b3c0926f2212c47af970bbccc584e7af8bb7ac41
sosoariar <sosoariar@163.com>
2022-03-30 20:16:53 +0800

    自动测试构建

--- clean_code/src/test/java/com/soso/FileReaderTester.java
+++ clean_code/src/test/java/com/soso/FileReaderTester.java
@@ -2,10 +2,30 @@ package com.soso;
 
 import junit.framework.TestCase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FileReaderTester extends TestCase {
 
     public FileReaderTester(String name){
         super(name);
     }
 
+    protected void setUp(){
+        try{
+            input = new FileReader("data.txt");
+        }catch(FileNotFoundException e){
+            throw new RuntimeException("unable to open test file");
+        }
+    }
+
+    protected void tearDown(){
+        try{
+            input.close();
+        }catch(Exception e){
+            throw new RuntimeException("error on closing test file");
+        }
+    }
+
+    private FileReader input;
 }
