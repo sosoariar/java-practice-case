bfd4c437693b71895e35d5bfb0f709088369051e
sosoariar <sosoariar@163.com>
2022-03-30 20:07:48 +0800

    新建测试函数

---
+++ clean_code/src/test/java/com/soso/FileReaderTester.java
@@ -0,0 +1,11 @@
+package com.soso;
+
+import junit.framework.TestCase;
+
+public class FileReaderTester extends TestCase {
+
+    public FileReaderTester(String name){
+        super(name);
+    }
+
+}
