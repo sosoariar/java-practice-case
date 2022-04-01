ff9cece19cebdab49a7dd3176c2ff859d83b47bc
sosoariar <sosoariar@163.com>
2022-04-01 19:18:58 +0800

    Dao 层测试类

---
+++ IPresistece_test/src/main/java/com/soso/bean/IUserDao.java
@@ -0,0 +1,13 @@
+package com.soso.bean;
+
+import java.util.List;
+
+public interface IUserDao {
+
+    // 查询所有用户
+    public List<User> findAll() throws Exception;
+
+    // 根据条件进行用户查询
+    public User findByCondition(User user);
+
+}
---
+++ IPresistece_test/src/main/java/com/soso/bean/UserDaoImpl.java
@@ -0,0 +1,36 @@
+package com.soso.bean;
+
+import com.soso.io.Resources;
+import com.soso.sqlSession.SqlSession;
+import com.soso.sqlSession.SqlSessionFactory;
+import com.soso.sqlSession.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class UserDaoImpl implements IUserDao{
+
+    @Override
+    public List<User> findAll() throws Exception {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<User> users = sqlSession.selectList("user.selectList");
+        for(User user1:users){
+            System.out.println(user1);
+        }
+        return users;
+    }
+
+    @Override
+    public User findByCondition(User user) throws Exception{
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user2 = sqlSession.selectOne("user.selectOne",user);
+        System.out.println(user2);
+        return null;
+    }
+
+}
