9c6dd3b26808e8020db4e576f4a6fd08ed70b4b3
sosoariar <sosoariar@163.com>
2022-04-01 20:50:12 +0800

    Mybatis MySQL Test

---
+++ mybatis-case/src/test/java/com/soso/MybatisTest.java
@@ -0,0 +1,28 @@
+package com.soso;
+
+import com.soso.bean.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+
+    @Test
+    public void test1() throws IOException{
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> users = sqlSession.selectList("user.findAll");
+        for(User user:users){
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+}
