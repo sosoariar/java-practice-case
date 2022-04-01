358f1b161579cfbd19d9a66a5c6ddcca8bde3d6d
sosoariar <sosoariar@163.com>
2022-04-01 16:25:44 +0800

    完成对象实例和parametertypeClass的映射

--- IPresistence/src/main/java/com/soso/sqlSession/Executor.java
+++ IPresistence/src/main/java/com/soso/sqlSession/Executor.java
@@ -4,9 +4,10 @@ import com.soso.pojo.Configuration;
 import com.soso.pojo.MappedStatement;
 
 import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface Executor {
-    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object ... params) throws SQLException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IntrospectionException;
+    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object ... params) throws SQLException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IntrospectionException, InvocationTargetException;
 }
--- IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
+++ IPresistence/src/main/java/com/soso/sqlSession/SimpleExecutor.java
@@ -10,6 +10,7 @@ import com.soso.utils.ParameterMappingTokenHandler;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.*;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.List;
 public class SimpleExecutor implements Executor{
 
     @Override
-    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object... params) throws SQLException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IntrospectionException {
+    public <E> List<E> query(Configuration configuration, MappedStatement mappedStatement, Object... params) throws SQLException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException, IntrospectionException, InvocationTargetException {
         // 注册驱动,获取链接
         Connection connection = configuration.getDataSource().getConnection();
         // 获取SQL语句: select * from user where id = #{id} and username = #{username}
@@ -32,6 +33,7 @@ public class SimpleExecutor implements Executor{
         
         List<ParameterMapping> parameterMappingList = boundSql.getParameterMappingList();
         for(int i=0; i<parameterMappingList.size();i++){
+
             ParameterMapping parameterMapping = parameterMappingList.get(i);
             String content = parameterMapping.getContent();
 
@@ -47,7 +49,7 @@ public class SimpleExecutor implements Executor{
         // 执行SQL
         ResultSet resultSet = preparedStatement.executeQuery();
         String resultType = mappedStatement.getResultType();
-        Class<?> resultTypeClass = getClassType(resultType)
+        Class<?> resultTypeClass = getClassType(resultType);
 
         // 封装返回结果集
         while(resultSet.next()){
@@ -57,16 +59,18 @@ public class SimpleExecutor implements Executor{
                 // 字段名
                 String columnName = metaData.getColumnName(i);
                 // 字段的值
-                Object object = resultSet.getObject(columnName);
+                Object value = resultSet.getObject(columnName);
                 // 使用反射,根据数据库表和实体的对应关系,完成封装
                 PropertyDescriptor propertyDescriptor = new PropertyDescriptor(columnName,resultTypeClass);
                 Method writMethod = propertyDescriptor.getWriteMethod();
-                writMethod.invoke(o,value);
+                writMethod.invoke(resultTypeClass,value);
             }
         }
 
         return null;
     }
+
+
 
     private Class<?> getClassType(String parameterType) throws ClassNotFoundException {
         if(parameterType!=null){
