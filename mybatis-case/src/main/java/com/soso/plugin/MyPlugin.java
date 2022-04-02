b2486196bd4559bb20f15f8e7b852778ff213c99
sosoariar <sosoariar@163.com>
2022-04-02 14:28:01 +0800

    Mybatis 配置拦截对象

---
+++ mybatis-case/src/main/java/com/soso/plugin/MyPlugin.java
@@ -0,0 +1,33 @@
+package com.soso.plugin;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(type= StatementHandler.class,
+                method = "prepare",
+                args = {Connection.class,Integer.class})
+})
+public class MyPlugin implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return null;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
